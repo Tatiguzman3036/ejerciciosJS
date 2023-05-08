fe45dbcb7dd3a5387bfa2c0277bb09def90ad754
Tatiguzman3036 <tatianaguadalupeguzmn@hotmail.com>
2023-05-08 05:26:22 -0300

    segunda version

---
+++ bucles.js
@@ -0,0 +1,79 @@
+//ejercicio N° 1
+/* let numeroIngresado = prompt (`Ingrese el numero:`) */
+/* for (let i = 1; i <= 10 ; i++) {
+    console.log(`${i} x ${numeroIngresado} = ${i * numeroIngresado}`);
+} */
+//ejercicio N° 2
+/* for (let i = numeroIngresado; i >= 0; i--){
+    console.log(i);
+} */
+//Ejercicio N° 5
+/* let divisor = []
+for (let i = 1; i <= numeroIngresado; i++){
+    if (numeroIngresado % i === 0 ){
+        divisor.push(i)
+    }
+}
+ */
+/* console.log(divisor); */
+//ejercicio N° 6:
+/* let divisor = [1,2,3,4,5,6,7,8,9,10]
+for (let i = 0; i <= divisor.length; i++){
+    console.log(divisor[i]);
+}
+
+console.log(divisor);
+//ejercicio N° 7
+let lista = [1,2,3,4,5,6,7,8,9,10]
+for (let i = 0; i <= lista.length; i++){
+    console.log(`Su resultado es ${lista[i] * 2}`);
+}
+
+console.log(lista); */
+//ejercicio N° 8
+/* const familia = [{
+"nombre": "Cesar",
+"integrante": "Papa",
+"edad": 46,
+"trabajo": "policia",
+
+},{
+    "nombre": "Gabriela",
+    "integrante": "mama",
+    "edad": 46,
+    "trabajo":"policia",
+    
+},{
+        "nombre": "Camila",
+        "integrante": "hija",
+        "edad": 19,
+        "trabajo": "estudiante",
+        
+},{
+        "nombre":"Federico",
+        "integrante":"hijo",
+        "edad": 23,
+        "trabajo": "empleado",
+        
+},{
+        "nombre": "Franco",
+        "integrante": "hijo",
+        "edad": 16,
+        "trabajo": "estudiante",
+        
+},
+]
+for (let i = 0; i < familia.length; i++) {
+    console.log(`Su nombre es ${familia[i].nombre}, su rol en la familia es ser ${familia[i].integrante},
+    tiene ${familia[i].edad} y tiene como trabajo ${familia[i].trabajo}`);
+    
+} */
+//ejercicio N° 9
+/* const array = [1,2,3,4,5,6,7,8,9,10]
+for (let i = 1; i <= array.length; i++){
+    if (i % 2 == 0) {
+        console.log(i);
+    }
+} */
+//ejercicio N° 10
+let primerNumero = prompt (`Ingrese el numero`)
